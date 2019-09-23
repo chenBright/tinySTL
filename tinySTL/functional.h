1d96fb485269ca6294301556e5a9289aeaa6ac44
chenBright <1021774709@qq.com>
2019-09-24 02:50:50 +0800

    Feature: less_equal

--- tinySTL/functional.h
+++ tinySTL/functional.h
@@ -26,6 +26,13 @@ namespace tinySTL {
             return lhs < rhs;
         }
     };
+
+    template <class T>
+    struct less_equal : public binary_finction<T, T, bool> {
+        bool operator()(const T &lhs, const T &rhs) const {
+            return lhs <= rhs;
+        }
+    };
 }
 
 #endif //TINYSTL_FUNCTIONAL_H
