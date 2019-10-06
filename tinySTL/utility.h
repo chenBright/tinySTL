fca3649638e258e368af10f96d58bf44c49de961
chenBright <1021774709@qq.com>
2019-10-06 12:53:22 +0800

    Feature: forward

--- tinySTL/utility.h
+++ tinySTL/utility.h
@@ -5,6 +5,20 @@
 
 namespace tinySTL {
 
+    // 完美转发
+    // 返回类型使用了引用折叠技术：
+    // X& &、X& && 和 X&& & => X&
+    // X&& && => X&&
+    template <class T>
+    T&& forward(typename tinySTL::remove_reference<T>::type& t) noexcept {
+        return static_cast<T&&>(t);
+    }
+
+    template <class T>
+    T&& forward(typename tinySTL::remove_reference<T>::type&& t) noexcept {
+        return static_cast<T&&>(t);
+    }
+
     // 移动
     template <class T>
     typename tinySTL::remove_reference<T>::type&& move(T&& t) noexcept {
