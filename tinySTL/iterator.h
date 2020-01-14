1b1d715983f21a0b64d23b36ea20745b75c3eff7
chenBright <1021774709@qq.com>
2020-01-14 22:38:33 +0800

    Feature: back_inserter (#213)

---
+++ tinySTL/iterator.h
@@ -0,0 +1,17 @@
+#ifndef TINYSTL_ITERATOR_H
+#define TINYSTL_ITERATOR_H
+
+#include "iterator_base.h"
+#include "iterator_reverse_iterator.h"
+#include "iterator_back_insert_iterator.h"
+
+namespace tinySTL {
+
+    template <class Container>
+    back_insert_iterator<Container> back_inserter(Container& c) {
+        return back_insert_iterator<Container>(c);
+    }
+
+}
+
+#endif //TINYSTL_ITERATOR_H
